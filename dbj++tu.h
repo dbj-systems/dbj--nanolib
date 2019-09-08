df2496fa5df511924118e1783b0bc15fdbd2caf1
dbjdbj <dbj@dbj.org>
2019-09-08 16:56:56 +0200

    invalid nested comment  removed

--- dbj++tu.h
+++ dbj++tu.h
@@ -1,4 +1,3 @@
-
 #pragma once
 /*
 (c) 2019 by dbj.org
@@ -6,7 +5,7 @@
 Appache 2.0 licence. Please review the licence file in this project
 */
 
-#include "dbj_nanolib.h"
+#include "dbj++nanolib.h"
 
 /*
 testing nano-lib
@@ -15,10 +14,14 @@ Usage is with this single macro bellow.
 Testing unit is a lambda with no arguments.
 
 TU_REGISTER(
-    []{ /* testing code goes here */ }
+    []{ / * testing code goes here * / }
 );
 
-Place this wherever you like, but outside of functions or classes.
+void main() {
+    // execute all registered tests
+    dbj::tu::catalog.execute();
+}
+
 */
 
 #define TU_REGISTER inline auto _DBJ_CONCATENATE(dumsy_, __COUNTER__) = ::dbj::tu::catalog
@@ -26,106 +29,106 @@ Place this wherever you like, but outside of functions or classes.
 namespace dbj::tu
 {
 
-    using namespace std;
+using namespace std;
 
-    struct timer final
+struct timer final
+{
+    using buffer = array<char, 24>;
+    using timepoint = typename std::chrono::system_clock::time_point;
+    const timepoint start_ = std::chrono::system_clock::now();
+
+    double nano() const
     {
-        using buffer = array<char, 24>;
-        using timepoint = typename std::chrono::system_clock::time_point;
-        const timepoint start_ = std::chrono::system_clock::now();
+        timepoint end_ = std::chrono::system_clock::now();
+        return static_cast<double>((end_ - start_).count());
+    }
 
-        double nano() const
-        {
-            timepoint end_ = std::chrono::system_clock::now();
-            return static_cast<double>((end_ - start_).count());
-        }
+    double micro() const { return nano() / 1000.0; }
 
-        double micro() const { return nano() / 1000.0; }
+    double seconds() const { return micro() / 1000.0; }
 
-        double seconds() const { return micro() / 1000.0; }
+    double decimal3(double arg) { return (std::round(arg * 1000)) / 1000; }
 
-        double decimal3(double arg) { return (std::round(arg * 1000)) / 1000; }
-
-        enum class kind
-        {
-            nano,
-            micro,
-            second
-        };
-
-        friend buffer as_buffer(timer const &timer_, kind which_ = kind::micro)
-        {
-            buffer retval{char{0}};
-            double arg{};
-            char const *unit_{};
-            switch (which_)
-            {
-            case kind::nano:
-                arg = timer_.nano();
-                unit_ = " nano seconds ";
-                break;
-            case kind::micro:
-                arg = timer_.micro();
-                unit_ = " micro seconds ";
-                break;
-            default: //seconds
-                arg = timer_.seconds();
-                unit_ = " seconds ";
-            }
-            std::snprintf(retval.data(), retval.size(), "%.3f%s", arg, unit_);
-            return retval;
-        }
+    enum class kind
+    {
+        nano,
+        micro,
+        second
     };
 
-    constexpr inline static auto MSCVER = _MSC_VER;          // 1921
-    constexpr inline static auto MSCFULLVER = _MSC_FULL_VER; //192127702
-    constexpr inline static auto MSCBUILD = _MSC_BUILD;      // 2
-
-    using tu_function = void (*)();
-    using units_ = vector<tu_function>;
-
-    inline void line() noexcept { printf("\n----------------------------------------------------------------------"); }
-
-    struct testing_system final
+    friend buffer as_buffer(timer const &timer_, kind which_ = kind::micro)
     {
-        inline static units_ units{};
-
-        auto operator()(tu_function fun_) const
+        buffer retval{char{0}};
+        double arg{};
+        char const *unit_{};
+        switch (which_)
         {
-            units.push_back(fun_);
-            return fun_;
+        case kind::nano:
+            arg = timer_.nano();
+            unit_ = " nano seconds ";
+            break;
+        case kind::micro:
+            arg = timer_.micro();
+            unit_ = " micro seconds ";
+            break;
+        default: //seconds
+            arg = timer_.seconds();
+            unit_ = " seconds ";
         }
+        std::snprintf(retval.data(), retval.size(), "%.3f%s", arg, unit_);
+        return retval;
+    }
+};
 
-        void start() const
+constexpr inline static auto MSCVER = _MSC_VER;          // 1921
+constexpr inline static auto MSCFULLVER = _MSC_FULL_VER; //192127702
+constexpr inline static auto MSCBUILD = _MSC_BUILD;      // 2
+
+using tu_function = void (*)();
+using units_ = vector<tu_function>;
+
+inline void line() noexcept { printf("\n----------------------------------------------------------------------"); }
+
+struct testing_system final
+{
+    inline static units_ units{};
+
+    auto operator()(tu_function fun_) const
+    {
+        units.push_back(fun_);
+        return fun_;
+    }
+
+    void start() const
+    {
+        line();
+        printf("\n " _DBJ_STRINGIZE(MSCVER) " :\t\t %d", MSCVER);
+        printf("\n " _DBJ_STRINGIZE(MSCFULLVER) " :\t\t %d", MSCFULLVER);
+        printf("\n " _DBJ_STRINGIZE(MSCBUILD) " :\t\t %d", MSCBUILD);
+        printf("\n\n Catalogue has %zd test units", units.size());
+        line();
+    }
+
+    void end() const
+    {
+        printf("\n\n All tests done.\n\n");
+    }
+
+    void execute() const
+    {
+        start();
+        for (auto &tu_ : units)
         {
+            timer timer_{};
+            tu_();
             line();
-            printf("\n " _DBJ_STRINGIZE(MSCVER) " :\t\t %d", MSCVER);
-            printf("\n " _DBJ_STRINGIZE(MSCFULLVER) " :\t\t %d", MSCFULLVER);
-            printf("\n " _DBJ_STRINGIZE(MSCBUILD) " :\t\t %d", MSCBUILD);
-            printf("\n\n Catalogue has %zd test units", units.size());
+            printf("\nDone in: %s", as_buffer(timer_).data());
             line();
         }
+        end();
+    }
+};
 
-        void end() const
-        {
-            printf("\n\n All tests done.\n\n");
-        }
-
-        void execute() const
-        {
-            start();
-            for (auto &tu_ : units)
-            {
-                timer timer_{};
-                tu_();
-                line();
-                printf("\nDone in: %s", as_buffer(timer_).data());
-                line();
-            }
-            end();
-        }
-    };
-
-    constexpr inline testing_system catalog;
+constexpr inline testing_system catalog;
 
 } // namespace dbj::tu
